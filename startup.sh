db2aca0bc0a1ec13c0e5a632a47d843fa7ed22b4
Eduardo Silva <eduardo998468@gmail.com>
2026-08-08 08:54:20 -0300

    chore: change default port to 3000

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@ echo "Rodando migrações do Alembic..."
 alembic upgrade head
 
 echo "Iniciando servidor FastAPI com Uvicorn..."
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --proxy-headers --forwarded-allow-ips='*'
+exec uvicorn app.main:app --host 0.0.0.0 --port 3000 --proxy-headers --forwarded-allow-ips='*'
